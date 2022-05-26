23398bd5346f6f469395e066202b473dfe24bdcb
jhon tobon <jhon.shiru@gmail.com>
2022-05-26 16:34:27 -0500

    usltima config

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-let cont ;
+
 const cargarFace = ()=>{
-    let face='';
-    for(cont=0;cont<=10;cont++){
+    for(cont=0; cont<=11; cont++){
+
         let face = `<label>
         <input type="radio" name="green" />
         <div class="face"></div>
-      </label>`;
-        if(cont==true){
-            document.getElementById("lab").innerHTML=face.repeat(290);
-           
+        </label>`;
+        if(cont <=10){
+
+            document.getElementById("lab").innerHTML=face.repeat(300);
         }
-        
-    
+
+
     }
-   
-    
-}
+                
+         
+     }
+
